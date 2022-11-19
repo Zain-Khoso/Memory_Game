e94ceef3d43ae63385cd0abe30c8b03542fdf907
Zain <zain.khoso.dev@gmail.com>
2022-11-19 12:33:49 +0500

    Game_Play_Update
    -Added the Basic Game Experience Of Memory Game

--- script.js
+++ script.js
@@ -1,54 +1,111 @@
 Array.prototype.random = function () {
-    return this.splice(Math.floor((Math.random()*this.length)), 1)[0]
-}
+    return this.splice(Math.floor(Math.random() * this.length), 1)[0];
+};
 Array.prototype.setupPaths = function () {
-    this.push('imgs/cat.jpg')
-    this.push('imgs/elephant.jpg')
-    this.push('imgs/lion.png')
-    this.push('imgs/cat.jpg')
-    this.push('imgs/elephant.jpg')
-    this.push('imgs/lion.png')
-}
-const imgPaths = []
+    this.push("imgs/cat.jpg");
+    this.push("imgs/elephant.jpg");
+    this.push("imgs/lion.png");
+    this.push("imgs/cat.jpg");
+    this.push("imgs/elephant.jpg");
+    this.push("imgs/lion.png");
+};
+const imgPaths = [];
 const clkdDivs = [];
 
 let trys = 1;
-let attempts = 0;
+let attempts = 3;
 let score = 0;
 
 function clicked(node) {
-    if (trys <= 2) {
-        if (trys == 1) {
-            setupNode(node);
+    if (attempts > 1) {
+        if (trys <= 2) {
+            if (trys == 1) {
+                setupNode(node);
 
-            trys++;
-        } else if (trys == 2 && clkdDivs.includes(node)) {
-            alert("Don't try to be smart, Just play the game");
-            resetEnv();
-            trys = 1;
-        } else {
-            setupNode(node);
-            if (
-                clkdDivs[0].firstElementChild.src ==
-                clkdDivs[1].firstElementChild.src
-            ) {
-                score++;
-                attempts++;
-                src.textContent = `Score: ${score}`;
+                trys++;
+            } else if (trys == 2 && clkdDivs.includes(node)) {
+                alert("Don't try to be smart, Just play the game");
+                resetEnv();
+                --attempts;
                 atpt.textContent = `Attempts: ${attempts}`;
-
-                setTimeout(function () {
-                    alert("Congrets, You have won the game.");
-                }, 100);
-                setTimeout(resetEnv, 500);
             } else {
-                attempts++;
-                atpt.textContent = `Attempts: ${attempts}`;
+                setupNode(node);
+                if (
+                    clkdDivs[0].firstElementChild.src ==
+                    clkdDivs[1].firstElementChild.src
+                ) {
+                    score++;
+                    attempts = 3;
+                    src.textContent = `Score: ${score}`;
+                    atpt.textContent = `Attempts: ${attempts}`;
 
-                setTimeout(function () {
-                    alert("Game Over, Try again.");
-                }, 100);
-                setTimeout(resetEnv, 500);
+                    setTimeout(function () {
+                        alert("Congrets, You have won the game.");
+                    }, 100);
+                    setTimeout(function () {
+                        resetEnv();
+                        imgPaths.setupPaths();
+                        setupEnv();
+                    }, 500);
+                } else {
+                    --attempts;
+                    atpt.textContent = `Attempts: ${attempts}`;
+
+                    setTimeout(function () {
+                        alert(`Nice Try, You have ${attempts} Attempts left.`);
+                    }, 100);
+                    setTimeout(resetEnv, 500);
+                }
+            }
+        }
+    } else {
+        if (trys <= 2) {
+            if (trys == 1) {
+                setupNode(node);
+
+                trys++;
+            } else if (trys == 2 && clkdDivs.includes(node)) {
+                alert("You wasted Your Last Atempt");
+                resetEnv();
+                imgPaths.setupPaths();
+                setupEnv();
+
+                attempts = 3;
+                score = 0;
+                atpt.textContent = `Attempts: ${attempts}`;
+                src.textContent = `Score: ${score}`;
+            } else {
+                setupNode(node);
+                if (
+                    clkdDivs[0].firstElementChild.src ==
+                    clkdDivs[1].firstElementChild.src
+                ) {
+                    score++;
+                    attempts = 3;
+                    src.textContent = `Score: ${score}`;
+                    atpt.textContent = `Attempts: ${attempts}`;
+
+                    setTimeout(function () {
+                        alert("Congrets, You have won the game.");
+                    }, 100);
+                    setTimeout(function () {
+                        resetEnv();
+                        imgPaths.setupPaths();
+                        setupEnv();
+                    }, 500);
+                } else {
+                    setTimeout(function () {
+                        alert("Nice try, Better Luck Next Time.");
+                        resetEnv();
+                        imgPaths.setupPaths();
+                        setupEnv();
+                    }, 100);
+
+                    attempts = 3;
+                    score = 0;
+                    atpt.textContent = `Attempts: ${attempts}`;
+                    src.textContent = `Score: ${score}`;
+                }
             }
         }
     }
@@ -68,17 +125,14 @@ function resetEnv() {
     sItem.firstElementChild.width = 0;
     sItem.firstElementChild.height = 0;
 
-    imgPaths.setupPaths()
-    setupEnv()
-
     trys = 1;
 }
 
 function setupEnv() {
     for (let child of container.children) {
-        child.firstElementChild.src = imgPaths.random()
+        child.firstElementChild.src = imgPaths.random();
     }
 }
 
-imgPaths.setupPaths()
-setupEnv()
+imgPaths.setupPaths();
+setupEnv();
